46f9be8601a723697b09fbaf276332bb3c6e65d5
Ash Walker <ashurstwalker@gmail.com>
2022-12-09 13:22:51 -0500

    refactor: move nvimtree setup to own file

--- lua/signal/plugins/ui/init.lua
+++ lua/signal/plugins/ui/init.lua
@@ -169,99 +169,7 @@ function M.dap_ui()
     dap.listeners.after.event_exited['dapui_config'] = function() dapui.close({}) end
 end
 
-function M.nvimtree()
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-    vim.opt.termguicolors = true
-    require 'nvim-tree'.setup {
-        disable_netrw = true,
-        hijack_netrw = true,
-        open_on_setup = true,
-        open_on_setup_file = false,
-        ignore_ft_on_setup = { 'startify', 'alpha' },
-        ignore_buf_on_tab_change = {},
-        auto_reload_on_write = true,
-        create_in_closed_folder = true,
-        sort_by = 'extension',
-        hijack_cursor = true,
-        root_dirs = {},
-        prefer_startup_root = false,
-        sync_root_with_cwd = true,
-        reload_on_bufenter = true,
-        respect_buf_cwd = false,
-        hijack_directories = {
-            enable = true,
-            auto_open = true,
-        },
-        update_focused_file = {
-            enable = true,
-            update_root = true,
-            ignore_list = {}
-        },
-        system_open = {
-            cmd = "",
-            args = {},
-        },
-        diagnostics = {
-            enable = true,
-            show_on_dirs = true,
-        },
-        git = {
-            enable = true,
-            ignore = true,
-            show_on_dirs = true,
-        },
-        filesystem_watchers = {
-            enable = true
-        },
-        remove_keymaps = false,
-        select_prompts = true,
-        view = {
-            adaptive_size = false,
-            centralize_selection = false,
-            hide_root_folder = false,
-        },
-        renderer = {
-            add_trailing = false,
-            group_empty = true,
-            full_name = true,
-            highlight_opened_files = 'name',
-            indent_markers = {
-                enable = false,
-            },
-            icons = {
-                webdev_colors = true,
-                git_placement = 'signcolumn',
-            }
-        },
-        filters = {
-            dotfiles = true,
-            exclude = { '.gitignore' }
-        },
-        actions = {
-            change_dir = {
-                enable = true,
-            },
-            open_file = {
-                quit_on_open = false,
-                resize_window = true,
-                window_picker = {
-                    enable = true,
-                }
-            },
-            remove_file = {
-                close_window = false,
-            },
-            use_system_clipboard = true,
-        },
-        log = {
-            enable = false,
-            truncate = true,
-        }
-    }
-    vim.keymap.set('n', '<Leader>tt', '<cmd>NvimTreeToggle<cr>', { desc = 'nvim-tree toggle' })
-    vim.keymap.set('n', '<Leader>tf', '<cmd>NvimTreeFindFile<cr>', { desc = 'nvim-tree find file' })
-end
+M.nvimtree = require('signal.plugins.ui.nvimtree')
 
 function M.project()
     require('project_nvim').setup {
@@ -287,7 +195,6 @@ function M.telescope()
     }
     -- keymaps
     local tsb = require 'telescope.builtin'
-    local tst = require 'telescope.themes'
 
     -- files
     vim.keymap.set('n', '<Leader>fff', tsb.find_files, { desc = 'tsb.find_files' })
---
+++ lua/signal/plugins/ui/nvimtree.lua
@@ -0,0 +1,93 @@
+return function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    vim.opt.termguicolors = true
+    require 'nvim-tree'.setup {
+        disable_netrw = true,
+        hijack_netrw = true,
+        open_on_setup = true,
+        open_on_setup_file = false,
+        ignore_ft_on_setup = { 'startify', 'alpha' },
+        ignore_buf_on_tab_change = {},
+        auto_reload_on_write = true,
+        create_in_closed_folder = true,
+        sort_by = 'extension',
+        hijack_cursor = true,
+        root_dirs = {},
+        prefer_startup_root = false,
+        sync_root_with_cwd = true,
+        reload_on_bufenter = true,
+        respect_buf_cwd = false,
+        hijack_directories = {
+            enable = true,
+            auto_open = true,
+        },
+        update_focused_file = {
+            enable = true,
+            update_root = true,
+            ignore_list = {}
+        },
+        system_open = {
+            cmd = "",
+            args = {},
+        },
+        diagnostics = {
+            enable = true,
+            show_on_dirs = true,
+        },
+        git = {
+            enable = true,
+            ignore = true,
+            show_on_dirs = true,
+        },
+        filesystem_watchers = {
+            enable = true
+        },
+        remove_keymaps = false,
+        select_prompts = true,
+        view = {
+            adaptive_size = false,
+            centralize_selection = false,
+            hide_root_folder = false,
+        },
+        renderer = {
+            add_trailing = false,
+            group_empty = true,
+            full_name = true,
+            highlight_opened_files = 'name',
+            indent_markers = {
+                enable = false,
+            },
+            icons = {
+                webdev_colors = true,
+                git_placement = 'signcolumn',
+            }
+        },
+        filters = {
+            dotfiles = true,
+            exclude = { '.gitignore' }
+        },
+        actions = {
+            change_dir = {
+                enable = true,
+            },
+            open_file = {
+                quit_on_open = false,
+                resize_window = true,
+                window_picker = {
+                    enable = true,
+                }
+            },
+            remove_file = {
+                close_window = false,
+            },
+            use_system_clipboard = true,
+        },
+        log = {
+            enable = false,
+            truncate = true,
+        }
+    }
+    vim.keymap.set('n', '<Leader>tt', '<cmd>NvimTreeToggle<cr>', { desc = 'nvim-tree toggle' })
+    vim.keymap.set('n', '<Leader>tf', '<cmd>NvimTreeFindFile<cr>', { desc = 'nvim-tree find file' })
+end
