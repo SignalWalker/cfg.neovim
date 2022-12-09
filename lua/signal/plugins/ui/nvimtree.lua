575136c4f96daae481efe44a4a16b21ce5c03ffc
Ash Walker <ashurstwalker@gmail.com>
2022-12-09 13:39:23 -0500

    feat(nvimtree): don't resize on file open

--- lua/signal/plugins/ui/nvimtree.lua
+++ lua/signal/plugins/ui/nvimtree.lua
@@ -75,7 +75,7 @@ return function()
             },
             open_file = {
                 quit_on_open = false,
-                resize_window = true,
+                resize_window = false,
                 window_picker = {
                     enable = true,
                 }
