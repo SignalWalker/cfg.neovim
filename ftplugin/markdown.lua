8be9a97a00822129850924045f432cc8271abb2d
Ash Walker <ashurstwalker@gmail.com>
2023-10-14 16:04:52 -0400

    ...

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -2,3 +2,14 @@ local glow_ok, _ = pcall(require, 'glow')
 if glow_ok then
 	vim.keymap.set('n', '<Leader>bp', '<cmd>Glow<cr>', { desc = "preview markdown file" })
 end
+
+vim.opt.wrap = true
+
+vim.keymap.set('n', 'j', 'gj', { remap = false })
+vim.keymap.set('n', 'k', 'gk', { remap = false })
+
+vim.keymap.set('n', '<Up>', 'gk', { remap = false })
+vim.keymap.set('n', '<Down>', 'gj', { remap = false })
+
+vim.keymap.set('i', '<Up>', '<C-o>gk', { remap = false })
+vim.keymap.set('i', '<Down>', '<C-o>gj', { remap = false })
