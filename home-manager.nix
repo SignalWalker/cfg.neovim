ac6b9eff7dc63c52f480e162f45956257bbae849
Ash Walker <ashurstwalker@gmail.com>
2025-05-24 11:03:09 -0400

    2025-05-24T11:03-04:00

--- home-manager.nix
+++ home-manager.nix
@@ -1,12 +1,15 @@
-{self, ...}: {
+{ self, ... }:
+{
   config,
   pkgs,
   lib,
   ...
 }:
-with builtins; let
+with builtins;
+let
   ash = config.programs.ashvim;
-in {
+in
+{
   options = with lib; {
     programs.ashvim = {
       enable = mkEnableOption "Ash Walker's Neovim configuration";
@@ -33,142 +36,147 @@ in {
       };
     };
   };
-  disabledModules = [];
-  imports = [];
-  config = lib.mkIf ash.enable (lib.mkMerge [
-    {
-      programs.ashvim = {
-        vscodeExtensions = [
-          pkgs.vscode-extensions.vadimcn.vscode-lldb # nvim-dap
-        ];
-      };
-      home.file = {
-        "${ash.vscodeExtDir}".source = let
-          subDir = "share/vscode/extensions";
-          combinedExtDrv = pkgs.buildEnv {
-            name = "neovim-vscode-extensions";
-            paths = ash.vscodeExtensions;
-          };
-        in "${combinedExtDrv}/${subDir}";
-      };
-      programs.neovim = {
-        enable = true;
-        viAlias = true;
-        vimAlias = true;
-        vimdiffAlias = true;
-        defaultEditor = true;
-        package = ash.package;
-        extraPackages = with pkgs; [
-          luarocks # lazy-rocks
-          lua5_1 # lazy-rocks
+  disabledModules = [ ];
+  imports = [ ];
+  config = lib.mkIf ash.enable (
+    lib.mkMerge [
+      {
+        programs.ashvim = {
+          vscodeExtensions = [
+            pkgs.vscode-extensions.vadimcn.vscode-lldb # nvim-dap
+          ];
+        };
+        home.file = {
+          "${ash.vscodeExtDir}".source =
+            let
+              subDir = "share/vscode/extensions";
+              combinedExtDrv = pkgs.buildEnv {
+                name = "neovim-vscode-extensions";
+                paths = ash.vscodeExtensions;
+              };
+            in
+            "${combinedExtDrv}/${subDir}";
+        };
+        programs.neovim = {
+          enable = true;
+          viAlias = true;
+          vimAlias = true;
+          vimdiffAlias = true;
+          defaultEditor = true;
+          package = ash.package;
+          extraPackages = with pkgs; [
+            luarocks # lazy-rocks
+            lua5_1 # lazy-rocks
 
-          universal-ctags
+            universal-ctags
 
-          tree-sitter
+            tree-sitter
 
-          glow # markdown previews
-          fzf
+            glow # markdown previews
+            fzf
 
-          cmake # telescope-fzf-native
-          gnumake # telescope-fzf-native
-          clang
+            cmake # telescope-fzf-native
+            gnumake # telescope-fzf-native
+            clang
 
-          imagemagick # 3rd/image.nvim
-          delta # debugloop/telescope-undo.nvim
-          graphviz # rustaceanvim
-          taplo # toml formatting / language server
-          vscode-extensions.vadimcn.vscode-lldb.adapter # nvim-dap
+            imagemagick # 3rd/image.nvim
+            delta # debugloop/telescope-undo.nvim
+            graphviz # rustaceanvim
+            taplo # toml formatting / language server
+            vscode-extensions.vadimcn.vscode-lldb.adapter # nvim-dap
 
-          nixd # nix language server
+            nixd # nix language server
 
-          tailwindcss-language-server # css language server
+            tailwindcss-language-server # css language server
 
-          vscode-langservers-extracted # json, among other things
+            vscode-langservers-extracted # json, among other things
 
-          trash-cli # nvim-tree trash
+            trash-cli # nvim-tree trash
 
-          cmake-language-server
+            cmake-language-server
 
-          lua-language-server
-          stylua # lua formatting
+            lua-language-server
+            stylua # lua formatting
 
-          eslint
-          typescript-language-server
+            eslint
+            typescript-language-server
 
-          # snacks.nvim
-          sqlite # picker
+            # snacks.nvim
+            sqlite # picker
 
-          nixfmt-rfc-style
+            nixfmt-rfc-style
 
-          stylelint
-        ];
-        # python
-        withPython3 = true;
-        extraPython3Packages = pyPkgs: [];
-        # lua
-        extraLuaPackages = luaPkgs: [
-          luaPkgs.magick # 3rd/image.nvim
-        ];
+            stylelint
+          ];
+          # python
+          withPython3 = true;
+          extraPython3Packages = pyPkgs: [ ];
+          # lua
+          extraLuaPackages = luaPkgs: [
+            luaPkgs.magick # 3rd/image.nvim
+          ];
 
-        coc.enable = false;
-      };
+          coc.enable = false;
+        };
 
-      programs.kitty.extraConfig = ''
-        ## kitty-scrollback.nvim
-        # action_alias kitty_scrollback_nvim kitten ${config.xdg.dataHome}/nvim/lazy/kitty-scrollback.nvim/python/kitty_scrollback_nvim.py
-        # map kitty_mod+/ kitty_scrollback_nvim
-        # map kitty_mod+alt+/ kitty_scrollback_nvim --config ksb_builtin_last_cmd_output
+        programs.kitty.extraConfig = ''
+          ## kitty-scrollback.nvim
+          # action_alias kitty_scrollback_nvim kitten ${config.xdg.dataHome}/nvim/lazy/kitty-scrollback.nvim/python/kitty_scrollback_nvim.py
+          # map kitty_mod+/ kitty_scrollback_nvim
+          # map kitty_mod+alt+/ kitty_scrollback_nvim --config ksb_builtin_last_cmd_output
 
-        ## smart-splits.nvim
-        map alt+h neighboring_window left
-        map alt+j neighboring_window down
-        map alt+k neighboring_window up
-        map alt+l neighboring_window right
+          ## smart-splits.nvim
+          map alt+h neighboring_window left
+          map alt+j neighboring_window down
+          map alt+k neighboring_window up
+          map alt+l neighboring_window right
 
-        map --when-focus-on var:IS_NVIM alt+h
-        map --when-focus-on var:IS_NVIM alt+j
-        map --when-focus-on var:IS_NVIM alt+k
-        map --when-focus-on var:IS_NVIM alt+l
+          map --when-focus-on var:IS_NVIM alt+h
+          map --when-focus-on var:IS_NVIM alt+j
+          map --when-focus-on var:IS_NVIM alt+k
+          map --when-focus-on var:IS_NVIM alt+l
 
-        map kitty_mod+h neighboring_window left
-        map kitty_mod+j neighboring_window down
-        map kitty_mod+k neighboring_window up
-        map kitty_mod+l neighboring_window right
+          map kitty_mod+h neighboring_window left
+          map kitty_mod+j neighboring_window down
+          map kitty_mod+k neighboring_window up
+          map kitty_mod+l neighboring_window right
 
-        map ctrl+h kitten relative_resize.py left  3
-        map ctrl+j kitten relative_resize.py down  3
-        map ctrl+k kitten relative_resize.py up    3
-        map ctrl+l kitten relative_resize.py right 3
+          map ctrl+h kitten relative_resize.py left  3
+          map ctrl+j kitten relative_resize.py down  3
+          map ctrl+k kitten relative_resize.py up    3
+          map ctrl+l kitten relative_resize.py right 3
 
-        map --when-focus-on var:IS_NVIM ctrl+h
-        map --when-focus-on var:IS_NVIM ctrl+j
-        map --when-focus-on var:IS_NVIM ctrl+k
-        map --when-focus-on var:IS_NVIM ctrl+l
+          map --when-focus-on var:IS_NVIM ctrl+h
+          map --when-focus-on var:IS_NVIM ctrl+j
+          map --when-focus-on var:IS_NVIM ctrl+k
+          map --when-focus-on var:IS_NVIM ctrl+l
 
-        map kitty_mod+alt+shift+h kitten relative_resize.py left  3
-        map kitty_mod+alt+shift+j kitten relative_resize.py down  3
-        map kitty_mod+alt+shift+k kitten relative_resize.py up    3
-        map kitty_mod+alt+shift+l kitten relative_resize.py right 3
+          map kitty_mod+alt+shift+h kitten relative_resize.py left  3
+          map kitty_mod+alt+shift+j kitten relative_resize.py down  3
+          map kitty_mod+alt+shift+k kitten relative_resize.py up    3
+          map kitty_mod+alt+shift+l kitten relative_resize.py right 3
 
-      '';
-    }
-    (lib.mkIf (ash.configPath == null) {
-      xdg.configFile."nvim" = {
-        source = toString self;
-        recursive = false;
-      };
-    })
-    (lib.mkIf (ash.configPath != null) {
-      home.activation."make-neovim-cfg-symlink" = lib.hm.dag.entryAfter ["WriteBoundary"] ''
-        run rm $VERBOSE_ARG -- ${config.xdg.configHome}/nvim || true
-        run ln -s $VERBOSE_ARG -T ${ash.configPath} ${config.xdg.configHome}/nvim
-      '';
-    })
-    (lib.mkIf (config ? signal.dev.editor.editors) {
-      signal.dev.editor.editors."neovim" = {
-        cmd.term = "nvim";
-      };
-    })
-  ]);
-  meta = {};
+        '';
+      }
+      (lib.mkIf (ash.configPath == null) {
+        xdg.configFile."nvim" = {
+          source = toString self;
+          recursive = false;
+        };
+      })
+      (lib.mkIf (ash.configPath != null) {
+        home.activation."make-neovim-cfg-symlink" = lib.hm.dag.entryAfter [ "WriteBoundary" ] ''
+          run rm $VERBOSE_ARG -- ${config.xdg.configHome}/nvim || true
+          run ln -s $VERBOSE_ARG -T ${ash.configPath} ${config.xdg.configHome}/nvim
+        '';
+      })
+      (lib.mkIf (config ? signal.dev.editor.editors) {
+        home.packages = with pkgs; [ httm ];
+        signal.dev.editor.editors."neovim" = {
+          cmd.term = "ounce --trace nvim";
+        };
+      })
+    ]
+  );
+  meta = { };
 }
